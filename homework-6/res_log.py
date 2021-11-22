95157edc01deeaf9348fa6507841f66f83329405
Dashori <daahaaa@icloud.com>
2021-11-22 22:37:29 +0300

    fixed res_log and delete empty files

--- homework-6/res_log.py
+++ homework-6/res_log.py
@@ -1,4 +1,3 @@
-
 def total_count():
     return 225133
 
@@ -13,17 +12,6 @@ def total_post():
     
 def total_put():
     return 6
-
-def top_10():
-    temp_dict = {}
-    for request in requests:
-        url = request[6]
-        if url in temp_dict:
-            temp_dict[url] += 1
-        else:
-            temp_dict[url] = 1
-    return temp_dict
-
 
 def top_10_request():
     dict = {'/administrator/index.php' : 103932, '/apache-log/access.log' : 26336, "'/'" : 6940,
@@ -40,7 +28,7 @@ def top_4xx_big():
     c = (404, 1417, '189.217.45.73', '/index.php?option=com_phocagallery&view=category&id=%28SELECT%20%28CASE%20WHEN%20%289168%3D4696%29%20THEN%209168%20ELSE%209168%2A%28SELECT%209168%20FROM%20INFORMATION_SCHEMA.CHARACTER_SETS%29%20END%29%29&Itemid=53')
     d = (404, 1417, '189.217.45.73', '/index.php?option=com_phocagallery&view=category&id=%28SELECT%20%28CASE%20WHEN%20%281753%3D1753%29%20THEN%201753%20ELSE%201753%2A%28SELECT%201753%20FROM%20INFORMATION_SCHEMA.CHARACTER_SETS%29%20END%29%29&Itemid=53')
     e = (404, 1397, '104.129.9.248', '/index.php?option=com_easyblog&view=dashboard&layout=write')
-    arr = [b,a,c,d,e]
+    arr = [a,b,c,d,e]
     return arr
 
 def top_5xx():
